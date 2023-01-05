c7fe94f76ae58503bf51b51d1b5f36ef09164747
GlassPainter <ckwong6599@gmail.com>
2023-01-05 13:02:52 -0500

    1/5/2023

--- API-Project/main.js
+++ API-Project/main.js
@@ -1,4 +1,4 @@
-const URL = "https://api.quotable.io/random";
+/* const URL = "https://api.quotable.io/random";
 
 async function getData(URL) {
   try {
@@ -6,6 +6,23 @@ async function getData(URL) {
     console.log(data.content);
     document.getElementById("app").textContent = data.content;
   } catch (error) {}
+}
+
+getData(URL);
+ */
+
+const URL = "https://api.exchangerate.host/latest";
+
+async function getData(URL) {
+  try {
+    const data = await (await fetch(URL)).json();
+    console.log(data.rates);
+    data.forEach((element) => {
+      document.getElementById("app").textContent = element.rates;
+    });
+  } catch (error) {
+    console.log("error");
+  }
 }
 
 getData(URL);
