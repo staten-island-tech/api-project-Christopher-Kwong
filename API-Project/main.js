d41235c3fbef0eb6056d3610f4ff9cc3d4aa266a
GlassPainter <ckwong6599@gmail.com>
2023-01-20 14:01:10 -0500

    1/20/2023

--- API-Project/main.js
+++ API-Project/main.js
@@ -1,15 +1,15 @@
 const URL = "https://api.exchangerate.host/latest";
 
-let data = await (await fetch(URL)).json();
-
 const DOM = {
   output: document.querySelector("#output"),
-  rankings: document.querySelector("#rankings"),
+  outputRankings: document.querySelector(".outputRankings"),
   dropDownBase: document.querySelector(".dropDownBase"),
   dropDownConverted: document.querySelector(".dropDownConverted"),
   leastValued: document.querySelector(".leastValued"),
   mostValued: document.querySelector(".mostValued"),
 };
+
+let data = await (await fetch(URL)).json();
 
 async function createDropDown(arrayElement, ID) {
   Object.keys(arrayElement).forEach((element) =>
@@ -64,13 +64,35 @@ document.querySelector("#form").addEventListener("submit", (event) => {
 
 let entries = Object.entries(data.rates);
 
-function sortingValues() {
+function sortingValues(compareFunction) {
+  DOM.outputRankings.textContent = "";
+  for (let count = 0; count < 25; count++) {
+    entries.sort(compareFunction);
+    DOM.outputRankings.insertAdjacentHTML(
+      "beforeend",
+      `<p>${[count + 1]}: ${entries[count][0]}</p>`
+    );
+  }
+}
+
+DOM.leastValued.addEventListener("click", function () {
+  sortingValues((a, b) => b[1] - a[1]);
+});
+
+DOM.mostValued.addEventListener("click", function () {
+  sortingValues((a, b) => a[1] - b[1]);
+});
+
+/* function sortingValues() {
   for (let count = 0; count < 25; count++) {
     entries.sort((a, b) => a[1] - b[1]);
-    DOM.rankings.insertAdjacentHTML("beforeend", `<p>${entries[count][0]}</p>`);
+    DOM.rankings.insertAdjacentHTML(
+      "beforeend",
+      `<p>${[count + 1]}: ${entries[count][0]}</p>`
+    );
   }
 }
 
 DOM.leastValued.addEventListener("click", function () {
   sortingValues();
-});
+}); */
